b272eb4cf758f2c52cc4048c8ab3022ba2fe8af7
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-04-12 14:29:22 -0500

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,11 @@
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('Fizz Buzz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else if (i % 3 === 0) {
-        console.log('Fizz');
-    } else {
-        console.log(i);
+    let output = ""
+    if (i % 3 == 0) {
+        output += "Fizz";
+    } if (i % 5 == 0) {
+        output += "Buzz";
+    } if (output == "") {
+        output += i;
     }
+    console.log(output);
 }
